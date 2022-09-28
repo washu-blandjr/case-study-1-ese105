d9529f7d47f42e22cac0c005e2758ac07b5be980
Gokuf <Gokuf@MrBland_PDA.dhcp.wustl.edu>
2022-09-28 16:38:31 -0500

    9-28 Changelog
    -added header
    -added load function and kmeans function

--- Case_Study_1.m
+++ Case_Study_1.m
@@ -1,1 +1,12 @@
-%disregard this, it is a test
+%{ 
+Case Study 1
+
+Name for Collaborators
+----------------------
+Carmen Bland Jr
+Matthew Kuchak
+Maritza Mateo
+%} 
+
+load("COVIDbyCounty.mat");         %
+[cluster_9, c] = kmeans(CNTY_COVID,9,Replicates=10);
