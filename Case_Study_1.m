e39bbca22248164cd880877849db498b22f41cf8
Gokuf <Gokuf@MrBland_PDA.dhcp.wustl.edu>
2022-10-02 15:03:35 -0500

    10/2 Changelog
    -added division matrices
    -made subplots of division 1 and division 2

--- Case_Study_1.m
+++ Case_Study_1.m
@@ -10,7 +10,26 @@ Maritza Mateo
 
 load("COVIDbyCounty.mat");         
 
-[cluster_9, centroid] = kmeans(CNTY_COVID,9,Replicates=10);   %creates 9 clusters and their centroids
+div_1_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 1,:);
+div_1_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 1,:);
+div_2_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 2,:);
+div_2_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 2,:);
+div_3_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 3,:);
+div_3_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 3,:);
+div_4_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 4,:);
+div_4_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 4,:);
+div_5_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 5,:);
+div_5_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 5,:);
+div_6_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 6,:);
+div_6_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 6,:);
+div_7_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 7,:);
+div_7_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 7,:);
+div_8_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 8,:);
+div_8_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 8,:);
+div_9_covid = CNTY_COVID(CNTY_CENSUS.DIVISION == 9,:);
+div_9_census = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 9,:);
+
+[cluster_9, centroid] = kmeans(CNTY_COVID,9,'Replicates',10);   %creates 9 clusters and their centroids
 
 %define groups by name and non covid data
 cntygroup_1 = CNTY_CENSUS(cluster_9 == 1,:);
@@ -35,4 +54,29 @@ covidgroup_8 = CNTY_COVID(cluster_9 == 8,:);
 covidgroup_9 = CNTY_COVID(cluster_9 == 9,:);
 
 %issue so far: some clusters has multiple divisions rather than only one
-%              common division (i.e. cntygroup2 has divisions 5,6,7,8,9)
+%              common division
+
+% For referencing what a Division looks like (to comment out code,
+% highlight all code you wish to commentout and press Ctrl+R
+
+New_Eng_CNTYNAME = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 1,"CTYNAME");
+New_Eng_COVID = CNTY_COVID(CNTY_CENSUS.DIVISION == 1,:);
+
+Mid_Atl_CNTYNAME = CNTY_CENSUS(CNTY_CENSUS.DIVISION == 2,"CTYNAME");
+Mid_Atl_COVID = CNTY_COVID(CNTY_CENSUS.DIVISION == 2,:);
+
+figure;
+
+subplot(2,1,1);
+plot(dates,New_Eng_COVID);
+title("New England Covid Cases");
+xlabel("Date");
+ylabel("Cases");
+legend(New_Eng_CNTYNAME.Variables);
+
+subplot(2,1,2);
+plot(dates,Mid_Atl_COVID);
+title("Middle Atlantic Covid Cases");
+xlabel("Date");
+ylabel("Cases");
+legend(Mid_Atl_CNTYNAME.Variables);
